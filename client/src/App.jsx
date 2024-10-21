f596c5805f7ee03161c1f51437ab4323530e1713
nitinkumar09 <kumarnitin83086@gmail.com>
2024-10-22 01:04:21 +0530

    react and tailwind and create first template

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="">
+      <h1 className="text-3xl text-red-400 font-bold underline p-10">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
