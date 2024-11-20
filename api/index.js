f23ad3c4fda52f773d87c6be61359836db70baf1
nitinkumar09 <kumarnitin83086@gmail.com>
2024-11-21 02:55:24 +0530

    add update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRoutes from "./routes/user.route.js"
 import authRoutes from "./routes/auth.route.js"
+import cookieParser from "cookie-parser";
+
 import cors from "cors"
 dotenv.config();
 
@@ -12,8 +14,12 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(error)
 });
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "http://your-frontend-domain.com", // Update with your frontend URL
+    credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
