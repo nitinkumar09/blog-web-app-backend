96382167a1cd6fa41cb59edae4a81fedb8c35a2d
nitinkumar09 <kumarnitin83086@gmail.com>
2024-12-07 23:02:01 +0530

    add create a post  api route

---
+++ api/routes/post.route.js
@@ -0,0 +1,8 @@
+import express from "express"
+const router = express.Router();
+import { verifyToken } from "../utils/verifyUser.js"
+import { create } from "../controllers/post.controller.js";
+
+router.post("/create", verifyToken, create)
+
+export default router;
