6001a2e209e1f1da5f6252e01d47b30eab2a98c7
nitinkumar09 <kumarnitin83086@gmail.com>
2025-04-04 08:33:38 +0530

    add update post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 const router = express.Router();
 import { verifyToken } from "../utils/verifyUser.js"
-import { create, deletepost, getposts } from "../controllers/post.controller.js";
+import { create, deletepost, getposts, updatepost } from "../controllers/post.controller.js";
 
 router.post("/create", verifyToken, create);
 router.get("/getposts", getposts);
 router.delete('/deletepost/:postId/:userId', verifyToken, deletepost);
+router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
 
 export default router;
--- client/src/pages/UpdatePosts.jsx
+++ client/src/pages/UpdatePosts.jsx
@@ -1,19 +1,47 @@
 import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { app } from "../firebase";
 import { CircularProgressbar } from "react-circular-progressbar"
 import 'react-circular-progressbar/dist/styles.css'
-import { useNavigate } from "react-router-dom";
-export default function CreatePost() {
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+export default function UpdatePosts() {
     const [file, setFile] = useState(null);
     const [imageUploadProgress, setImageUploadProgress] = useState(null);
     const [imageUploadError, setImageUploadError] = useState(null);
     const [formData, setFormData] = useState({});
     const [publishError, setPublishError] = useState(null);
+    const { postId } = useParams();
     const navigate = useNavigate();
+    const {currentUser} = useSelector((state)=>state.user);
+    useEffect(() => {
+        try {
+            const fetchPost = async () => {
+                const res = await fetch(`http://localhost:3000/api/post/getposts?postId=${postId}`);
+
+                const data = await res.json();
+                if (!res.ok) {
+                    console.log(data.message);
+                    setPublishError(data.message);
+                    return;
+                }
+                if (res.ok) {
+                    setPublishError(null);
+                    setFormData(data.posts[0]);
+                }
+            };
+            fetchPost();
+        } catch (error) {
+            console.log(error.message);
+        }
+    }, [postId])
+
+
+
+
     const handleUploadImage = async () => {
         try {
             if (!file) {
@@ -56,8 +84,8 @@ export default function CreatePost() {
         e.preventDefault();
         try {
             const token = localStorage.getItem("token"); // Retrieve the token from localStorage
-            const res = await fetch("http://localhost:3000/api/post/create", {
-                method: "POST",
+            const res = await fetch(`http://localhost:3000/api/post/updatepost/${formData._id}/${currentUser._id}`, {
+                method: "PUT",
                 headers: {
                     "Content-Type": "application/json",
                     "Authorization": `Bearer ${token}` // Include the token in the Authorization header
@@ -80,14 +108,15 @@ export default function CreatePost() {
     }
     return (
         <div className="p-3 max-w-3xl mx-auto min-h-screen">
-            <h1 className="text-3xl text-center my-7 font-semibold">Create a post</h1>
+            <h1 className="text-3xl text-center my-7 font-semibold">Update post</h1>
             <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
                 <div className="flex flex-col gap-4 sm:flex-row justify-between">
-                    <TextInput type="text" placeholder="Title" required id="title" className="flex-1" onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
+                    <TextInput type="text" placeholder="Title" required id="title" className="flex-1" onChange={(e) => setFormData({ ...formData, title: e.target.value })} value={formData.title} />
                     <Select
                         onChange={(e) =>
                             setFormData({ ...formData, category: e.target.value })
                         }
+                        value={formData.category}
                     >
                         <option value="uncategorized">Select a category</option>
                         <option value="javascript">JavaScript</option>
@@ -122,7 +151,7 @@ export default function CreatePost() {
                 {formData.image && (
                     <img src={formData.image} alt="upload" className="w-full h-72 object-cover" />
                 )}
-                <ReactQuill theme="snow" placeholder="Write something..." className="h-72 mb-12" required
+                <ReactQuill theme="snow" value={formData.content} placeholder="Write something..." className="h-72 mb-12" required
                     onChange={
                         (value) => {
                             setFormData({ ...formData, content: value });
@@ -130,7 +159,7 @@ export default function CreatePost() {
                     }
                 />
                 <Button type="submit" gradientDuoTone="purpleToPink">
-                    Publish
+                    Update post
                 </Button>
                 {
                     publishError && <Alert className="mt-5" color="failure">{publishError}</Alert>
