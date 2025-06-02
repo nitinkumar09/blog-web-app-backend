7cee308a4f99b1c50f29dbe5e1027bcb1e68eec1
nitinkumar09 <kumarnitin83086@gmail.com>
2025-06-02 20:46:37 +0530

    add port

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ app.use(cors({
 
 app.use(cookieParser());
 app.use(express.json());
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
